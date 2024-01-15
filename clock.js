34f16ca5b8e01cd7fb6818c9ec3a155cc8cbde6c
edwin2011-la <edwinakobeh789@gmail.com>
2024-01-14 18:58:22 -0800

    complete

--- clock.js
+++ clock.js
@@ -6,3 +6,6 @@ function workingClock(){
     const secs = now.getSeconds().toString().padStart(2 ,'0');
     clock.textContent = `${hours}:${mins}:${secs}`;
 }
+
+setInterval(workingClock, 1000);
+workingClock();
