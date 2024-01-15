b64399e9c5f032eb54f6b27d22499665a2040da1
edwin2011-la <edwinakobeh789@gmail.com>
2024-01-14 18:51:09 -0800

    js

--- clock.js
+++ clock.js
@@ -1,1 +1,8 @@
-
+function workingClock(){
+    const clock = document.querySelector('.clock');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2 ,'0');
+    const mins = now.getMinutes().toString().padStart(2 ,'0');
+    const secs = now.getSeconds().toString().padStart(2 ,'0');
+    clock.textContent = `${hours}:${mins}`
+}
